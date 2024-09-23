70097d8de595df836ed896ad2a4c3cc36c450ca5
humanshun <human.kagoshima.shindomeshunsuke@gmail.com>
2024-09-23 16:52:58 +0900

    同じ惑星がぶつかったら進化する機能追加

--- Supernova/Assets/Scripts/Planet.cs
+++ Supernova/Assets/Scripts/Planet.cs
@@ -18,6 +18,7 @@ public enum PLANETS_TYPE
 
 public class Planet : MonoBehaviour
 {
+    public GameObject nextPlanet;
     // 各Planetのプレハブが入った配列
     public GameObject[] planetPrefabs;
     public PLANETS_TYPE planetType;
@@ -29,52 +30,16 @@ public class Planet : MonoBehaviour
             // 同じenum同士が衝突した場合
             if (planet.planetType == planetType)
             {
-                // 現在のplanetTypeの一つ上を取得
-                PLANETS_TYPE nextPlanetType = GetNextPlanetType(planetType);
-
-                // 新しいPlanetを生成
-                CreateNewPlanet(nextPlanetType);
-
-                // このオブジェクトを破壊
+                // 相手のPlanetのみを破壊
                 Destroy(gameObject);
+
+                collision.gameObject.GetComponent<Planet>().nextPlanet = null;
+
+                if (nextPlanet)
+                {
+                    Instantiate(nextPlanet, transform.position, transform.rotation);
+                }
             }
-        }
-    }
-
-    // 次のPLANETS_TYPEを取得するヘルパー関数
-    private PLANETS_TYPE GetNextPlanetType(PLANETS_TYPE currentType)
-    {
-        int nextTypeIndex = (int)currentType + 1;
-
-        // enumの範囲を超えないようにチェック
-        if (nextTypeIndex > (int)PLANETS_TYPE.taiyou)
-        {
-            nextTypeIndex = (int)PLANETS_TYPE.taiyou; // taiyou以上は存在しないのでtaiyouに固定
-        }
-
-        return (PLANETS_TYPE)nextTypeIndex;
-    }
-
-    // 新しいPlanetオブジェクトを生成する関数
-    private void CreateNewPlanet(PLANETS_TYPE planetType)
-    {
-        // プレハブが設定されているか、planetTypeの範囲内にあるかチェック
-        int prefabIndex = (int)planetType - 1; // enumのインデックスに対応するため -1
-
-        if (planetPrefabs != null && prefabIndex >= 0 && prefabIndex < planetPrefabs.Length)
-        {
-            GameObject newPlanet = Instantiate(planetPrefabs[prefabIndex], transform.position, Quaternion.identity);
-            Planet newPlanetScript = newPlanet.GetComponent<Planet>();
-
-            // 新しいPlanetにplanetTypeをセット
-            if (newPlanetScript != null)
-            {
-                newPlanetScript.planetType = planetType;
-            }
-        }
-        else
-        {
-            Debug.LogError("プレハブが設定されていないか、インデックスが範囲外です。");
         }
     }
 }
